f50aa3584d5b7c5a124b84e5c0f3acbbf0bdb961
Lorenzo van der Woude <lorenzovanderwoude@hotmail.com>
2025-01-19 15:11:32 +0100

    Renames file to better match contents and adds functionality for retrieving polls

--- api/polls.py
+++ api/polls.py
@@ -26,3 +26,22 @@ class PollApp():
         req = requests.get(url)
         return req.json()['token']
 
+    def get_polls(self, amount: int = 10, difficulty: str = "medium",
+                  category: int = None, question_type: str = None) -> list:
+        req = ""
+        url = f"https://opentdb.com/api.php?amount={amount}&difficulty={difficulty}"
+        if category:
+            url += f"&category={category}"
+        if question_type:
+            url += f"&question_type={question_type}"
+
+        try:
+            req = requests.get(url)
+        except requests.exceptions.RequestException as e:
+            print(e)
+        finally:
+            response = req.json()
+            if response["response_code"] == 0:
+                polls_list = [poll for poll in response["results"]]
+                return polls_list
+
