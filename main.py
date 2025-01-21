13949698ed667392bf954ab98688da3ed9eb01bc
Lorenzo van der Woude <lorenzovanderwoude@hotmail.com>
2025-01-21 17:48:20 +0100

    Changes structure of answering poll questions

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-import random
 from api.polls import PollApp
 from helper.formatting import single_poll_from_list, format_categories
-from helper.answering import parse_answer, format_int_in_range, format_difficulty
+from helper.answering import parse_answer, format_int_in_range, format_difficulty, poll_handler
 
 
 def main():
     # TODO: Add exception handling for user input.
-    # TODO: Add documentation
-    # TODO: Cleaning up main loop by breaking up code into smaller methods
     # TODO: Fix token getting reset every time the game is restarted
     app = PollApp()
     user_input = None
@@ -34,31 +31,27 @@ def main():
                         user_category = category['id']
                 break
 
-        user_difficulty_answer = format_difficulty("Please select a difficulty level: ")
+        user_difficulty_answer = format_difficulty("Please select a difficulty level: ").lower()
         user_amount_answer = format_int_in_range("Please enter the amount of poll questions you'd like to answer: ")
         print("\nA random poll will now be generated for you...")
 
         polls_response = app.get_polls(category=user_category,
                                        difficulty=user_difficulty_answer, amount=user_amount_answer)
         while polls_response:
-            single_poll = single_poll_from_list(polls_response)
-            print(f"Your question is: {single_poll['question']}")
-            print("The answers are: ")
-            for answer in single_poll['answers']:
-                print(f"\t{answer}")
-            user_answer = ""
-            while True:
-                user_answer = input("Choose wisely, what do you think the right answer is? ")
-                if user_answer not in single_poll['answers']:
-                    print("Please enter a valid answer from the list of answers.")
-                else:
-                    if user_answer == single_poll['correct_answer']:
-                        print(f"You chose {single_poll['correct_answer']}, this was correct!")
-                        break
-                    else:
-                        print(f"You chose {user_answer}, this was incorrect!")
-                        continue
-        active = False
+            poll_details = single_poll_from_list(polls_response)
+            poll = poll_handler(poll_details)
+
+        while True:
+            user_continue_answer = input("\nWould you like to continue? (y/n): ").lower()
+            if user_continue_answer == "y":
+                active = True
+                break
+            elif user_continue_answer == "n":
+                active = False
+                break
+            else:
+                print("Please enter either 'y' or 'n': ")
+                continue
 
     print("Thanks for playing!")
 
