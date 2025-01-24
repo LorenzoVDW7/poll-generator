7f0f469a06c0e7c1d8ed61e7b4233b1662b837c0
Lorenzo van der Woude <lorenzovanderwoude@hotmail.com>
2025-01-24 17:30:34 +0100

    Adds feedback to console about .env I/O

--- helper/identifier.py
+++ helper/identifier.py
@@ -1,13 +1,15 @@
 import uuid
-from dotenv import load_dotenv, set_key
+from dotenv import set_key
 import os
 
 def generate_id():
     return str(uuid.uuid4())
 
 def get_or_create_identifier():
+    # TODO: fix error where 'set_key' isn't writing to the .env file
     user_uuid = os.getenv("USER_UUID")
     if not user_uuid:
         user_uuid = generate_id()
         set_key("../.env","USER_UUID", user_uuid)
+        print("UUID written to .env")
     return user_uuid
