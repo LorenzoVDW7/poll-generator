337d33b54921a328ce3c44ddfac6c1e00bc6ff60
Lorenzo van der Woude <lorenzovanderwoude@hotmail.com>
2025-02-07 16:22:52 +0100

    adds default values to config file

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ load_dotenv()
 class Config:
     """Centralized configuration"""
 
-    DB_USER = os.getenv("DATABASE_USERNAME")
-    DB_PASSWORD = os.getenv("DATABASE_PASSWORD")
-    DB_HOST = os.getenv("DATABASE_HOST")
+    DB_USER = os.getenv("DATABASE_USERNAME", "root")
+    DB_PASSWORD = os.getenv("DATABASE_PASSWORD", "")
+    DB_HOST = os.getenv("DATABASE_HOST", "127.0.0.1")
     DB_PORT = os.getenv("DATABASE_PORT")
-    DB_NAME = os.getenv("DATABASE_DB")
+    DB_NAME = os.getenv("DATABASE_DB", "polling")
