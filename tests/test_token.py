2f2490e6151f7d549a4439cadb58fc9567470fd9
Lorenzo van der Woude <lorenzovanderwoude@hotmail.com>
2025-02-07 15:37:52 +0100

    Reverts code back to old working form since code wasn't the issue

--- tests/test_token.py
+++ tests/test_token.py
@@ -1,9 +1,7 @@
 """The tests for the API token reside in this module.
 TODO: Add tests when token is being stored inside of a database."""
-from api.polls import PollApp
 
 
-def test_token():
-    app = PollApp()
+def test_token(app):
     token = app.token
     assert token
