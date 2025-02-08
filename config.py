253503e035d8df28d39dfe3b662e054206678a25
Lorenzo van der Woude <lorenzovanderwoude@hotmail.com>
2025-02-08 13:34:50 +0100

    Changes .env database values

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ load_dotenv()
 class Config:
     """Centralized configuration"""
 
-    DB_USER = os.getenv("DATABASE_USERNAME", "root")
-    DB_PASSWORD = os.getenv("DATABASE_PASSWORD", "")
-    DB_HOST = os.getenv("DATABASE_HOST", "127.0.0.1")
-    DB_PORT = os.getenv("DATABASE_PORT")
-    DB_NAME = os.getenv("DATABASE_DB", "polling")
+    DB_USER = os.getenv("DB_USER", "root")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+    DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
+    DB_PORT = os.getenv("DB_PORT", "3306")
+    DB_NAME = os.getenv("DB_NAME", "polling")
